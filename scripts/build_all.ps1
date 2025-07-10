b456dc0cf4ec202abec570a1039709ae42808286
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:00:52 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "EOTS_EP1_1.0" `
+  -ModName "Eye of the Storm Episode One 1.0" `
+  -ModFolder "eots" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/eye-of-the-storm/EOTS-EP1-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "214622e91a186987d36220919e2d62a0b4cd3b319805047da3fc9f3822f8e3c4" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
